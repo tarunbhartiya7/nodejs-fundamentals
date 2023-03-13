b9c00967f90af52e468b2ce75d5472d0186b3391
Tarun Bhartiya <tarun.bhartiya@primark.com>
2023-03-13 16:42:48 +0530

    task 2.2 completed

--- module-2/controllers/user.js
+++ module-2/controllers/user.js
@@ -1,10 +1,35 @@
 const userRouter = require('express').Router()
 const uuid = require('uuid')
+const Joi = require('joi')
+const { validateSchema } = require('../utils')
 
 let users = []
 
+const getAutoSuggestUsers = (loginSubstring, limit) => {
+  return users
+    .filter((item) => item.login.includes(loginSubstring))
+    .filter((item, i) => i <= limit - 1)
+    .sort((a, b) => (a.login < b.login ? -1 : 1))
+}
+
+const schema = Joi.object({
+  login: Joi.string().alphanum().min(3).max(30).required(),
+
+  password: Joi.string()
+    .min(3)
+    .max(30)
+    .required()
+    .pattern(new RegExp('^[a-zA-Z]+[0-9]+$')),
+
+  age: Joi.number().min(4).max(130).required(),
+})
+
 userRouter.get('/', (req, res) => {
-  res.send(users)
+  const limit = req.query.limit || 10
+  const substr = req.query.substr ?? ''
+
+  const result = getAutoSuggestUsers(substr, limit)
+  res.send(result)
 })
 
 userRouter.get('/:id', (req, res) => {
@@ -17,12 +42,12 @@ userRouter.get('/:id', (req, res) => {
   }
 })
 
-userRouter.post('/', (req, res) => {
-  const { username, password, age } = req.body
+userRouter.post('/', validateSchema(schema), (req, res) => {
+  const { login, password, age } = req.body
 
   const user = {
     id: uuid.v4(),
-    login: username,
+    login,
     password,
     age,
     isDeleted: false,
